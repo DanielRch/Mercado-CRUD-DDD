85b198b4b79d088cb508a7ea046b0fbd51d612b6
DanielRch <69549252+DanielRch@users.noreply.github.com>
2021-08-22 12:00:50 -0300

    Entities creation

---
+++ Mercado.Domain/Entities/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Domain.Entities
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+
+        [Display (Name = "Nome do Cliente")]
+        public string NomeCliente { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
---
+++ Mercado.Domain/Entities/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Domain.Entities
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+
+        public string Setor { get; set; }
+
+        [Display(Name = "Nome do Produto")]
+        public string NomeProduto { get; set; }
+
+        public string Marca { get; set; }
+                
+        public double Valor { get; set; }
+
+        [Display(Name = "Data de Vencimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DataVencimento { get; set; }
+
+    }
+}
