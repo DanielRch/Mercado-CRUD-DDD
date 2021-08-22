e883cf2af4fd115b15ca2bf9ca173695498e3963
DanielRch <69549252+DanielRch@users.noreply.github.com>
2021-08-22 12:12:39 -0300

    Context creation

---
+++ Mercado.Infra/Context/MercadoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Mercado.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Infra.Context
+{
+    public class MercadoDbContext : DbContext
+    {
+        public MercadoDbContext(DbContextOptions options) : base(options)
+        {
+            Database.SetCommandTimeout((int)TimeSpan.FromMinutes(5).TotalSeconds);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
