226f76aa97d762a65a902d201a325d15c4f78370
DanielRch <69549252+DanielRch@users.noreply.github.com>
2021-08-22 12:25:17 -0300

    Entity primary keys configuration

---
+++ Mercado.Infra/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Mercado.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Infra.EntityConfig
+{
+    public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(x => x.IdCliente);
+        }
+    }
+}
---
+++ Mercado.Infra/EntityConfig/ProdutoConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Mercado.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Infra.EntityConfig
+{
+    public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.IdProduto);
+        }
+    }
+}
